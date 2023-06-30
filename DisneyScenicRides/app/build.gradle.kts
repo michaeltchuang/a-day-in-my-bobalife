5d3fb7bb3518ca304e71268ee435e3c3d406cec2
Michael Chuang <github@michaeltchuang.com>
2023-06-29 21:47:46 -0700

    ci: bump up extensions version

--- DisneyScenicRides/app/build.gradle.kts
+++ DisneyScenicRides/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
     kotlin {
         jvmToolchain(17)
