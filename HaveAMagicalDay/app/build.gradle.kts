d3cb547052721663d11caa4235eb5eb324a9f4fa
Michael T Chuang <github@michaeltchuang.com>
2025-08-18 15:25:41 -0700

    fix: lint issues

--- HaveAMagicalDay/app/build.gradle.kts
+++ HaveAMagicalDay/app/build.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Date
 import java.util.TimeZone
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.21"
+    id("org.jetbrains.kotlin.jvm") version "2.2.10"
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
     application
 }
